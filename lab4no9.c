bccabb6f49de0fbe3881b968aaf4a76d43bb6e5a
6704062610313 <s6704062610313@email.kmutnb.ac.th>
2025-12-25 23:48:42 +0700

    N grade version 3
    ข้อ 9

--- lab4no9.c
+++ lab4no9.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int score,n,i;
+    int score,n,i,a=0,b=0,c=0,d=0,f=0;
 
     while(1){
     	scanf("%d", &score);
@@ -12,22 +12,34 @@ int main() {
         		printf("error score\n");
     		}else if (score >= 85) {
                 printf("%d(A)",score);
+                a++;
 			}else if (score >= 75) {
                 printf("%d(B)",score);
+                b++;
 			}else {
 				printf("%d(C)",score);
+				c++;
         	}
     	}
     	else {
     	    if (score >= 55) {
     	        printf("%d(D)",score);
+    	        d++;
     		}else if (score < 0){
         	    printf("error score\n");
-        	}else
-				printf("%d(F)",score);
+        	}
+			else{
+        		printf("%d(F)",score);
+				f++;
+			}	
     	}
     printf("\n");
 	}
+	printf("A(%d)\n",a);
+	printf("B(%d)\n",b);
+	printf("C(%d)\n",c);
+	printf("D(%d)\n",d);
+	printf("F(%d)\n",f);
 
     return 0;
 }
